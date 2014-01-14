0cbb6f8631424b0399c3cfbb606b0fe863e6699b
Michael Kohn <mike@mikekohn.net>
2014-01-13 21:57:07 -0600

    Remove if/def code from assembler.c.

--- common/assembler.c
+++ common/assembler.c
@@ -47,6 +47,7 @@
 #include "disasm_tms9900.h"
 #include "disasm_z80.h"
 #include "directives_data.h"
+#include "directives_if.h"
 #include "eval_expression.h"
 #include "get_tokens.h"
 #include "ifdef_expression.h"
@@ -308,117 +309,6 @@ char token[TOKENLEN];
   get_token(asm_context, token, TOKENLEN);
 
   printf("Public symbol: %s (ignored)\n", token);
-
-  return 0;
-}
-
-static int ifdef_ignore(struct _asm_context *asm_context)
-{
-char token[TOKENLEN];
-int token_type;
-
-  while(1)
-  {
-    token_type=get_token(asm_context, token, TOKENLEN);
-
-    if (token_type==TOKEN_EOF)
-    {
-      print_error("Missing endif", asm_context);
-      return -1;
-    }
-
-    if (token_type==TOKEN_EOL)
-    {
-      asm_context->line++;
-    }
-      else
-    if (token_type==TOKEN_POUND || IS_TOKEN(token,'.'))
-    {
-      token_type=get_token(asm_context, token, TOKENLEN);
-      if (strcasecmp(token, "endif")==0) return 0;
-        else
-      if (strcasecmp(token, "else")==0) return 2;
-    }
-  }
-}
-
-static int parse_ifdef_ignore(struct _asm_context *asm_context, int ignore_section)
-{
-  if (ignore_section==1)
-  {
-    if (ifdef_ignore(asm_context)==2)
-    {
-      assemble(asm_context);
-    }
-  }
-    else
-  {
-    if (assemble(asm_context)==2)
-    {
-      ifdef_ignore(asm_context);
-    }
-  }
-
-  return 0;
-}
-
-static int parse_ifdef(struct _asm_context *asm_context, int ifndef)
-{
-char token[TOKENLEN];
-int token_type;
-int ignore_section=0;
-int param_count; // throw away
-
-  asm_context->ifdef_count++;
-
-  asm_context->parsing_ifdef=1;
-  token_type=get_token(asm_context, token, TOKENLEN);
-  asm_context->parsing_ifdef=0;
-
-  if (token_type!=TOKEN_STRING)
-  {
-    print_error("#ifdef has no label", asm_context);
-    return -1;
-  }
-
-  if (defines_heap_lookup(&asm_context->defines_heap, token, &param_count)!=NULL)
-  {
-    if (ifndef==1) ignore_section=1;
-  }
-    else
-  {
-    if (ifndef==0) ignore_section=1;
-  }
-
-  parse_ifdef_ignore(asm_context, ignore_section);
-
-  asm_context->ifdef_count--;
-
-  return 0;
-}
-
-static int parse_if(struct _asm_context *asm_context)
-{
-int num;
-
-  asm_context->ifdef_count++;
-
-  asm_context->parsing_ifdef=1;
-  num=eval_ifdef_expression(asm_context);
-  asm_context->parsing_ifdef=0;
-
-  if (num==-1) return -1;
-
-  if (num!=0)
-  {
-    parse_ifdef_ignore(asm_context, 0);
-  }
-    else
-  {
-    parse_ifdef_ignore(asm_context, 1);
-  }
-
-  asm_context->ifdef_count--;
 
   return 0;
 }
