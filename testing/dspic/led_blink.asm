25181529d32f4a1c7a430192e81d757ca115270e
Michael Kohn <mike@mikekohn.net>
2014-02-02 09:38:41 -0600

    Adding a dsPIC33FJ06GS101A LED blinking test program. Fixing something really assy in the dsPIC30F3012 test program.

--- testing/dspic/led_blink.asm
+++ testing/dspic/led_blink.asm
@@ -8,7 +8,7 @@
 .org 0x100
 main:
   clr w0
-  mov.b wreg, TRISB 
+  mov.b wreg, TRISB
 repeat:
   mov #2, w0
   mov.b wreg, PORTB
@@ -16,14 +16,17 @@ repeat:
   call delay
 
   mov #1, w0
-  mov.b wreg, PORTB 
+  mov.b wreg, PORTB
 
   call delay
 
   bra repeat
 
 delay:
-  mov w4, 1000
+  ;; wow mike, wtf?  Probably w4 needs to be 0xffff to create the same
+  ;; delay.  I don't have a dsPIC3012 hooked up to test this right now.
+  ;mov w4, 1000
+  mov #0xffff, w4
 repeat_loop:
   dec w4, w4
   bra nz, repeat_loop
---
+++ testing/dspic/led_blink33.asm
@@ -0,0 +1,36 @@
+
+.dspic
+.include "p33fj06gs101a.inc"
+
+.org 0
+  goto main
+
+.org 0x100
+main:
+  clr w0
+  mov.b wreg, TRISA
+repeat:
+  mov #2, w0
+  mov.b wreg, PORTA
+
+  call delay
+
+  mov #1, w0
+  mov.b wreg, PORTA
+
+  call delay
+
+  bra repeat
+
+delay:
+  mov #5, w5
+repeat_loop_outer:
+  mov #0xffff, w4
+repeat_loop_inner:
+  dec w4, w4
+  bra nz, repeat_loop_inner
+  dec w5, w5
+  bra nz, repeat_loop_outer
+  return
+
+
