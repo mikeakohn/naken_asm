0ab5200c98fe0d7c65dadad9aeae67c68280691d
Michael Kohn <mike@mikekohn.net>
2014-02-23 17:05:27 -0600

    Change spacing of source code.

--- common/directives_if.c
+++ common/directives_if.c
@@ -27,41 +27,41 @@ int token_type;
 
   while(1)
   {
-    token_type=get_token(asm_context, token, TOKENLEN);
+    token_type = get_token(asm_context, token, TOKENLEN);
 
-    if (token_type==TOKEN_EOF)
+    if (token_type == TOKEN_EOF)
     {
       print_error("Missing endif", asm_context);
       return -1;
     }
 
-    if (token_type==TOKEN_EOL)
+    if (token_type == TOKEN_EOL)
     {
       asm_context->line++;
     }
       else
-    if (token_type==TOKEN_POUND || IS_TOKEN(token,'.'))
+    if (token_type == TOKEN_POUND || IS_TOKEN(token,'.'))
     {
-      token_type=get_token(asm_context, token, TOKENLEN);
-      if (strcasecmp(token, "endif")==0) return 0;
+      token_type = get_token(asm_context, token, TOKENLEN);
+      if (strcasecmp(token, "endif") == 0) return 0;
         else
-      if (strcasecmp(token, "else")==0) return 2;
+      if (strcasecmp(token, "else") == 0) return 2;
     }
   }
 }
 
 int parse_ifdef_ignore(struct _asm_context *asm_context, int ignore_section)
 {
-  if (ignore_section==1)
+  if (ignore_section == 1)
   {
-    if (ifdef_ignore(asm_context)==2)
+    if (ifdef_ignore(asm_context) == 2)
     {
       assemble(asm_context);
     }
   }
     else
   {
-    if (assemble(asm_context)==2)
+    if (assemble(asm_context) == 2)
     {
       ifdef_ignore(asm_context);
     }
@@ -74,16 +74,16 @@ int parse_ifdef(struct _asm_context *asm_context, int ifndef)
 {
 char token[TOKENLEN];
 int token_type;
-int ignore_section=0;
+int ignore_section = 0;
 int param_count; // throw away
 
   asm_context->ifdef_count++;
 
-  asm_context->parsing_ifdef=1;
-  token_type=get_token(asm_context, token, TOKENLEN);
-  asm_context->parsing_ifdef=0;
+  asm_context->parsing_ifdef = 1;
+  token_type = get_token(asm_context, token, TOKENLEN);
+  asm_context->parsing_ifdef = 0;
 
-  if (token_type!=TOKEN_STRING)
+  if (token_type != TOKEN_STRING)
   {
     print_error("#ifdef has no label", asm_context);
     return -1;
@@ -91,11 +91,11 @@ int param_count; // throw away
 
   if (macros_lookup(&asm_context->macros, token, &param_count)!=NULL)
   {
-    if (ifndef==1) ignore_section=1;
+    if (ifndef == 1) ignore_section = 1;
   }
     else
   {
-    if (ifndef==0) ignore_section=1;
+    if (ifndef == 0) ignore_section = 1;
   }
 
   parse_ifdef_ignore(asm_context, ignore_section);
@@ -111,13 +111,13 @@ int num;
 
   asm_context->ifdef_count++;
 
-  asm_context->parsing_ifdef=1;
-  num=eval_ifdef_expression(asm_context);
-  asm_context->parsing_ifdef=0;
+  asm_context->parsing_ifdef = 1;
+  num = eval_ifdef_expression(asm_context);
+  asm_context->parsing_ifdef = 0;
 
-  if (num==-1) return -1;
+  if (num == -1) return -1;
 
-  if (num!=0)
+  if (num != 0)
   {
     parse_ifdef_ignore(asm_context, 0);
   }
