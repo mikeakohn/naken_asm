26ba2c71ee29a460b2f2f33050b2a1401e60de9c
Michael Kohn <mike@mikekohn.net>
2014-11-03 18:51:04 -0600

    65xx: Added full automated test for all opcodes.

--- testing/comparison/gen_65xx.py
+++ testing/comparison/gen_65xx.py
@@ -4,8 +4,6 @@ import os
 
 def create_asm(instruction):
   out = open("temp.asm", "wb")
-  out.write("  cpu 6502\n")
-  out.write("  output hex\n")
   out.write("  *=0\n")
   out.write("  " + instruction + "\n")
   out.close()
@@ -17,20 +15,39 @@ out = open("65xx.txt", "wb")
 
 for instruction in fp:
   instruction = instruction.strip()
+  if instruction.startswith(";"): continue
   print instruction
   create_asm(instruction)
 
-  #os.system("xa temp.asm")
-  os.system("crasm -o temp.hex temp.asm > /dev/null")
+  os.system("xa temp.asm")
 
-  fp1 = open("temp.hex", "rb")
-  hex = fp1.readline().strip()
+  fp1 = open("a.o65", "rb")
+
+  hex = ""
+  code = ""
+  l = 0
+  checksum = 0
+
+  while 1:
+    a = fp1.read(1)
+    if a == "": break 
+    a = ord(a)
+    l += 1
+    checksum += a
+    code = code + ("%02X" % a)
+
+  code_len = "%02X" % l
+  checksum += l
+
+  checksum = ((checksum ^ 0xff) + 1) & 0xff
+  checksum = "%02X" % checksum
+  hex = ":" + code_len + "000000" + code + checksum
 
   out.write(instruction + "|" + hex + "\n")
 
   fp1.close
 
-  os.remove("temp.hex")
+  os.remove("a.o65")
 
 fp.close()
 out.close()
