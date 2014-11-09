0a6ad235c3c69e27479c76f4d55543c6b2c0c279
Mortis69 <joe_7@sbcglobal.net>
2014-11-09 10:21:34 -0600

    added 65816 table (not checked very well yet)

--- table/table_65816.h
+++ table/table_65816.h
@@ -14,6 +14,89 @@
 #ifndef _TABLE_65816_H
 #define _TABLE_65816_H
 
+enum
+{
+  // a
+  ABSOLUTE,
+
+  // (a,x)
+  ABSOLUTE_INDIRECT_X,
+
+  // (a)
+  ABSOLUTE_INDIRECT,
+
+  // al
+  ABSOLUTE_LONG,
+
+  // al,x
+  ABSOLUTE_LONG_X,
+
+  // a,x
+  ABSOLUTE_X,
+
+  // a,y
+  ABSOLUTE_Y,
+
+  // A
+  ACCUMULATOR,
+
+  // xyc
+  BLOCK_MOVE,
+
+  // d
+  DIRECT,
+
+  // (d,x)
+  DIRECT_INDIRECT_X,
+
+  // (d)
+  DIRECT_INDIRECT,
+
+  // (d),y
+  DIRECT_INDIRECT_Y,
+
+  // [d],y
+  DIRECT_INDIRECT_LONG_Y,
+
+  // [d]
+  DIRECT_INDIRECT_LONG,
+
+  // d,x
+  DIRECT_X,
+
+  // d,y
+  DIRECT_Y,
+
+  // #
+  IMMEDIATE,
+
+  // i
+  IMPLIED,
+
+  // r
+  RELATIVE,
+
+  // rl
+  RELATIVE_LONG,
+
+  // s
+  STACK,
+
+  // d,s
+  STACK_RELATIVE,
+
+  // (d,s),y
+  STACK_RELATIVE_INDIRECT_Y
+};
+
+struct _table_65816
+{
+  int opcode;
+  char *instruction;
+  int mode;
+  int cycles_min;
+  int cycles_max;
+};
 
 #endif
 
