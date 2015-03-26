76f680acac66c28f3e0e18e62f807d86cab5d775
Michael Kohn <mike@mikekohn.net>
2015-03-25 20:20:24 -0500

    680x0: Adding automated test for 680x0 (and hopefully other cpus) branches.

---
+++ testing/jump_test/jump_test.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import sys,os
+
+def test_jump(arch, instruction, distance, jump):
+
+  print instruction + " / " + jump + " ... ",
+
+  out = open("test.asm", "wb")
+
+  out.write("." + arch + "\n")
+  out.write("  " + jump + "\n")
+  out.write("  " + instruction + "\n")
+  out.write("blah:\n\n")
+  out.close()
+
+  os.system("../../naken_asm -l test.asm > /dev/null")
+
+  fp2 = open("out.hex")
+  code = fp2.readline()
+  fp2.close()
+
+  code = code[13:17]
+
+  if int(code,16) != distance:
+    print "\x1b[31mERROR! " + str(int(code,16)) + " " + str(distance) + "\x1b[0m"
+    return 1
+  else:
+    print "\x1b[32mPASS\x1b[0m"
+    return 0
+
+# ------------------------------- fold here ---------------------------------
+
+errors = 0
+
+if len(sys.argv) != 2:
+  print "Usage: jump_test.py <arch>"
+  sys.exit(0)
+
+arch = sys.argv[1]
+
+fp = open("../comparison/" + arch + ".txt", "rb")
+
+for line in fp:
+  tokens = line.strip().split("|")
+  code = tokens[1]
+  code = code[:-2]
+  code = code[9:]
+  distance = len(code) / 2
+  #print str(tokens) + " " + code + " " + str(distance)
+
+  errors += test_jump(arch, tokens[0], distance + 4, "jmp (blah)")
+  errors += test_jump(arch, tokens[0], distance, "jmp blah")
+  errors += test_jump(arch, tokens[0], distance, "beq blah")
+
+  break
+
+fp.close()
+
+print "Total errors " + str(errors)
+
