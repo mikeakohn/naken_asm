7eff9edfd2b1250110030e1f418b74ceef8ac446
Michael Kohn <mike@mikekohn.net>
2016-01-28 04:46:43 -0600

    Adding table for Propeller CPU.

---
+++ table/propeller.h
@@ -0,0 +1,38 @@
+/**
+ *  naken_asm assembler.
+ *  Author: Michael Kohn
+ *   Email: mike@mikekohn.net
+ *     Web: http://www.mikekohn.net/
+ * License: GPL
+ *
+ * Copyright 2010-2016 by Michael Kohn
+ *
+ */
+
+#ifndef _TABLE_PROPELLER_H
+#define _TABLE_PROPELLER_H
+
+#include <stdint.h>
+
+enum
+{
+  PROPELLER_OP_NONE,
+  PROPELLER_OP_DS,
+  PROPELLER_OP_S,
+  PROPELLER_OP_IMMEDIATE,
+};
+
+struct _table_propeller
+{
+  const char *instr;
+  uint32_t opcode;
+  uint32_t mask;
+  uint8_t operand_type;
+  uint8_t cycles_min;
+  uint8_t cycles_max;
+};
+
+extern struct _table_propeller table_propeller[];
+
+#endif
+
