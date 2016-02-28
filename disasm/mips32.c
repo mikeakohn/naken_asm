ce2dfb1aef115fac81dea687be306fe883b02b67
Michael Kohn <mike@mikekohn.net>
2016-02-28 07:42:09 -0600

    The $s0-$s7 registers were being shown as $a0-$a7 in disassembly.

--- disasm/mips32.c
+++ disasm/mips32.c
@@ -33,7 +33,7 @@ int disasm_mips32(struct _memory *memory, uint32_t address, char *instruction, i
   {
     "$0", "$at", "$v0", "$v1", "$a0", "$a1", "$a2", "$a3",
     "$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7",
-    "$a0", "$a1", "$a2", "$a3", "$a4", "$a5", "$a6", "$a7",
+    "$s0", "$s1", "$s2", "$s3", "$s4", "$s5", "$s6", "$s7",
     "$t8", "$t9", "$k0", "$k1", "$gp", "$sp", "$fp", "$ra"
   };
 
