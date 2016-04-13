6cf34c4b232b9c5786d517f25918be0affd0d3e8
Michael Kohn <mike@mikekohn.net>
2016-04-13 06:45:07 -0500

    Adding some output for lst file.

--- disasm/65816.c
+++ disasm/65816.c
@@ -34,17 +34,33 @@ int disasm_65816(struct _memory *memory, uint32_t address, char *instruction, in
 void list_output_65816(struct _asm_context *asm_context, uint32_t start, uint32_t end)
 {
 #if 0
-int cycles_min,cycles_max;
-char instruction[128];
-unsigned int opcode=get_opcode32(&asm_context->memory, address);
+  int cycles_min,cycles_max;
+  unsigned int opcode=get_opcode32(&asm_context->memory, address);
 
   opcode &= 0xFF;
 
   fprintf(asm_context->list, "\n");
   disasm_65816(&asm_context->memory, address, instruction, &cycles_min, &cycles_max);
+#endif
+  char instruction[128];
+  char bytes[32];
+  //uint8_t opcode = memory_read_m(&asm_context->memory, start)
+  int count = end - start;
+  int n;
 
-  fprintf(asm_context->list, "0x%04x: 0x%02x %-40s cycles: ", address, opcode, instruction);
+  strcpy(instruction, "???");
 
+  bytes[0] = 0;
+  for (n = 0; n < count; n++)
+  {
+    char temp[4];
+    sprintf(temp, "%02x ", memory_read_m(&asm_context->memory, start + n));
+    strcat(bytes, temp);
+  }
+
+  fprintf(asm_context->list, "0x%04x: 0x%-16s %-40s cycles: ?\n", start, bytes, instruction);
+
+#if 0
   if (cycles_min==cycles_max)
   { fprintf(asm_context->list, "%d\n", cycles_min); }
     else
