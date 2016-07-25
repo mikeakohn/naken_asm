3fec5e4f1968af25fd91632694a87b945fe3c706
Michael Kohn <mike@mikekohn.net>
2016-07-25 17:54:50 -0500

    Adding file to generate hex codes using the c51asm assembler for 8051 to test against the output of naken_asm.

---
+++ tests/comparison/gen_8051.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import os
+
+def create_asm(instruction):
+  out = open("temp.asm", "wb")
+  out.write("  " + instruction + "\n")
+  out.write("END\n")
+  out.close()
+
+# --------------------------------- fold here -------------------------------
+
+fp = open("8051_template.txt", "rb")
+out = open("8051.txt", "wb")
+
+for instruction in fp:
+  instruction = instruction.strip()
+  print instruction
+  create_asm(instruction)
+
+  os.system("c51asm temp.asm")
+
+  fp1 = open("temp.hex", "rb")
+  hex = fp1.readline().strip()
+  out.write(instruction + "|" + hex + "\n")
+  fp1.close
+
+
+  os.remove("temp.hex")
+
+fp.close()
+out.close()
+
+os.remove("temp.asm")
+
+
