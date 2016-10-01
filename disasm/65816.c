e88595ffca94fb852a4ec1eb04c965c193cb4b07
Michael Kohn <mike@mikekohn.net>
2016-10-01 13:46:54 -0500

    Adding some code for disassembly in the 65C816 module.

--- disasm/65816.c
+++ disasm/65816.c
@@ -19,34 +19,83 @@
 #include "disasm/65816.h"
 #include "table/65816.h"
 
+extern struct _table_65816 table_65816[];
+extern struct _table_65816_opcodes table_65816_opcodes[];
+
 int get_cycle_count_65816(uint16_t opcode)
 {
   return -1;
 }
 
-int disasm_65816(struct _memory *memory, uint32_t address, char *instruction, int *cycles_min, int *cycles_max)
+int disasm_65816(struct _memory *memory, uint32_t address, char *instruction, int *cycles_min, int *cycles_max, int bytes)
 {
-  return -1;
+  char temp[128];
+  uint8_t opcode = memory_read_m(memory, address);
+  int offset;
+
+  strcpy(instruction, table_65816[table_65816_opcodes[opcode].instr].name);
+
+  switch(table_65816_opcodes[opcode].op)
+  {
+    case OP_NONE:
+      break;
+    case OP_IMMEDIATE8:
+    case OP_IMMEDIATE16:
+    case OP_ADDRESS8:
+    case OP_ADDRESS16:
+    case OP_ADDRESS24:
+    case OP_INDEXED8_X:
+    case OP_INDEXED8_Y:
+    case OP_INDEXED16_X:
+    case OP_INDEXED16_Y:
+    case OP_INDEXED24_X:
+    case OP_INDIRECT8:
+    case OP_INDIRECT8_LONG:
+    case OP_INDIRECT16:
+    case OP_INDIRECT16_LONG:
+    case OP_X_INDIRECT8:
+    case OP_X_INDIRECT16:
+    case OP_INDIRECT8_Y:
+    case OP_INDIRECT8_Y_LONG:
+    case OP_BLOCK_MOVE:
+      strcat(instruction, " ???");
+      break;
+    case OP_RELATIVE:
+      offset = (int8_t)memory_read_m(memory, address + 1);
+      sprintf(temp, " 0x%04x (%d)", address + 2 + offset, offset);
+      strcat(instruction, temp);
+      break;
+    case OP_RELATIVE_LONG:
+    case OP_SP_RELATIVE:
+    case OP_SP_INDIRECT_Y:
+    default:
+      strcat(instruction, " ???");
+      break;
+  }
+
+  return 0;
 }
 
 void list_output_65816(struct _asm_context *asm_context, uint32_t start, uint32_t end)
 {
 #if 0
-  int cycles_min,cycles_max;
   unsigned int opcode=memory_read32_m(&asm_context->memory, address);
 
   opcode &= 0xFF;
 
   fprintf(asm_context->list, "\n");
-  disasm_65816(&asm_context->memory, address, instruction, &cycles_min, &cycles_max);
 #endif
+
   char instruction[128];
   char bytes[32];
+  int cycles_min,cycles_max;
   //uint8_t opcode = memory_read_m(&asm_context->memory, start)
   int count = end - start;
   int n;
 
-  strcpy(instruction, "???");
+  disasm_65816(&asm_context->memory, start, instruction, &cycles_min, &cycles_max, end - start + 1);
+
+  //strcpy(instruction, "???");
 
   bytes[0] = 0;
   for (n = 0; n < count; n++)
--- disasm/65816.h
+++ disasm/65816.h
@@ -19,7 +19,7 @@
 #include "common/assembler.h"
 
 int get_cycle_count_65816(uint16_t opcode);
-int disasm_65816(struct _memory *memory, uint32_t address, char *instruction, int *cycles_min, int *cycles_max);
+int disasm_65816(struct _memory *memory, uint32_t address, char *instruction, int *cycles_min, int *cycles_max, int bytes);
 void list_output_65816(struct _asm_context *asm_context, uint32_t start, uint32_t end);
 void disasm_range_65816(struct _memory *memory, uint32_t flags, uint32_t start, uint32_t end);
 
