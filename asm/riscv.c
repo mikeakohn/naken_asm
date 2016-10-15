290dcb7329ebfe708af0de7bd4bbcbc162bd357c
Michael Kohn <mike@mikekohn.net>
2016-10-15 15:19:26 -0500

    RISCV R instructions assemble.

--- asm/riscv.c
+++ asm/riscv.c
@@ -24,6 +24,8 @@
 enum
 {
   OPERAND_NONE,
+  OPERAND_X_REGISTER,
+  OPERAND_F_REGISTER,
 };
 
 struct _operand
@@ -32,49 +34,174 @@ struct _operand
   int type;
 };
 
-int parse_instruction_riscv(struct _asm_context *asm_context, char *instr)
+static int get_register_number(char *token)
 {
-char token[TOKENLEN];
-int token_type;
-char instr_case[TOKENLEN];
-struct _operand operands[3];
-int operand_count = 0;
-int matched = 0;
-int n;
+  int num;
 
-  lower_copy(instr_case, instr);
+  if (token[0] < '0' || token[0] > '9') { return -1; }
+  if (token[1] == 0) { return token[0] - '0'; }
+  if (token[0] == '0' || token[2] != 0) { return -1; }
+  if (token[1] < '0' || token[1] > '9') { return -1; }
 
-  memset(&operands, 0, sizeof(operands));
+  num = ((token[0] - '0') << 10) + (token[1] - '0');
+
+  return (num < 32) ? num : -1;
+}
+
+static int get_x_register_riscv(char *token)
+{
+  if (token[0] != 'x') { return -1; }
+
+  return get_register_number(token + 1);
+}
+
+static int get_f_register_riscv(char *token)
+{
+  if (token[0] != 'f') { return -1; }
+
+  return get_register_number(token + 1);
+}
+
+static int get_operands(struct _asm_context *asm_context, struct _operand *operands, char *instr, char *instr_case)
+{
+  char token[TOKENLEN];
+  int token_type;
+  int operand_count = 0;
+  int n;
+
   while(1)
   {
     token_type = tokens_get(asm_context, token, TOKENLEN);
-    if (token_type == TOKEN_EOL || token_type==TOKEN_EOF)
+    if (token_type == TOKEN_EOL || token_type == TOKEN_EOF)
     {
       break;
     }
 
     // FIXME - FILL IN
-
-    operand_count++;
+#if 0
     token_type = tokens_get(asm_context, token, TOKENLEN);
     if (token_type == TOKEN_EOL) break;
-    if (IS_NOT_TOKEN(token, ',') || operand_count == 3)
+    if (IS_NOT_TOKEN(token, ',') || operand_count == 5)
+    {
+      print_error_unexp(token, asm_context);
+      return -1;
+    }
+#endif
+
+    if (operand_count == 0 && IS_TOKEN(token, '.'))
+    {
+      strcat(instr_case, ".");
+      token_type = tokens_get(asm_context, token, TOKENLEN);
+      n = 0;
+      while(token[n] != 0) { token[n] = tolower(token[n]); n++; }
+      strcat(instr_case, token);
+      continue;
+    }
+
+    do
+    {
+      n = get_x_register_riscv(token);
+
+      if (n != -1)
+      {
+        operands[operand_count].type = OPERAND_X_REGISTER;
+        operands[operand_count].value = n;
+        break;
+      }
+
+      n = get_f_register_riscv(token);
+
+      if (n != -1)
+      {
+        operands[operand_count].type = OPERAND_F_REGISTER;
+        operands[operand_count].value = n;
+        break;
+      }
+
+    } while(0);
+
+    operand_count++;
+
+    token_type = tokens_get(asm_context, token, TOKENLEN);
+
+    if (token_type == TOKEN_EOL) { break; }
+
+    if (IS_NOT_TOKEN(token, ',') || operand_count == 5)
     {
       print_error_unexp(token, asm_context);
       return -1;
     }
   }
 
+  return operand_count;
+}
+
+int parse_instruction_riscv(struct _asm_context *asm_context, char *instr)
+{
+  char instr_case[TOKENLEN];
+  struct _operand operands[5];
+  int operand_count;
+  int matched = 0;
+  uint32_t opcode;
+  int n;
+
+  lower_copy(instr_case, instr);
+
+  memset(&operands, 0, sizeof(operands));
+
+  operand_count = get_operands(asm_context, operands, instr, instr_case);
+
+  if (operand_count < 0) { return -1; }
+
+
   n = 0;
   while(table_riscv[n].instr != NULL)
   {
     if (strcmp(table_riscv[n].instr,instr_case) == 0)
     {
-      matched=1;
+      matched = 1;
 
       switch(table_riscv[n].type)
       {
-        //case OP_BRANCH:
+        case OP_R_TYPE:
+          if (operand_count != 3)
+          {
+            print_error_opcount(instr, asm_context);
+            return -1;
+          }
+
+          if (operands[0].type != OPERAND_X_REGISTER ||
+              operands[1].type != OPERAND_X_REGISTER ||
+              operands[2].type != OPERAND_X_REGISTER)
+          {
+            print_error_illegal_operands(instr, asm_context);
+            return -1;
+          }
+
+          opcode = table_riscv[n].opcode |
+                  (operands[2].value << 20) |
+                  (operands[1].value << 15) |
+                  (operands[0].value << 7);
+          add_bin32(asm_context, opcode, IS_OPCODE);
+
+          return 4;
+        case OP_I_TYPE:
+        case OP_S_TYPE:
+        case OP_SB_TYPE:
+        case OP_U_TYPE:
+        case OP_UJ_TYPE:
+        case OP_SHIFT:
+        case OP_FENCE:
+        case OP_FFFF:
+        case OP_READ:
+        case OP_LR:
+        case OP_STD_EXT:
+        case OP_R_FP1:
+        case OP_R_FP2:
+        case OP_R_FP3:
+        case OP_R_FP2_RM:
+        case OP_R_FP3_RM:
+        case OP_R_FP4_RM:
         default:
           break;
       }
--- disasm/riscv.c
+++ disasm/riscv.c
@@ -16,9 +16,7 @@
 #include "disasm/riscv.h"
 #include "table/riscv.h"
 
-//#define READ_RAM(a) memory_read_m(memory, a)
-//#define READ_RAM16(a) (memory_read_m(memory, a)<<8)|memory_read_m(memory, a+1)
-#define READ_RAM(a) (memory_read_m(memory, a)<<24)|(memory_read_m(memory, a)<<16)|(memory_read_m(memory, a)<<8)|memory_read_m(memory, a+1)
+#define READ_RAM(a) (memory_read_m(memory, a+3)<<24)|(memory_read_m(memory, a+2)<<16)|(memory_read_m(memory, a+1)<<8)|memory_read_m(memory, a)
 
 int get_cycle_count_riscv(unsigned short int opcode)
 {
@@ -27,42 +25,84 @@ int get_cycle_count_riscv(unsigned short int opcode)
 
 int disasm_riscv(struct _memory *memory, uint32_t address, char *instruction, int *cycles_min, int *cycles_max)
 {
+  uint32_t opcode;
+  int n;
+
   *cycles_min = -1;
   *cycles_max = -1;
+
+  opcode = READ_RAM(address);
+
+  n = 0;
+  while(table_riscv[n].instr != NULL)
+  {
+    if ((opcode & table_riscv[n].mask) == table_riscv[n].opcode)
+    {
+      uint32_t rd = (opcode >> 7) & 0x1f;
+      uint32_t rs1 = (opcode >> 15) & 0x1f;
+      uint32_t rs2 = (opcode >> 20) & 0x1f;
+      const char *instr = table_riscv[n].instr;
+
+      switch(table_riscv[n].type)
+      {
+        case OP_R_TYPE:
+          sprintf(instruction, "%s r%d, r%d, r%d", instr, rd, rs1, rs2);
+          break;
+        case OP_I_TYPE:
+        case OP_S_TYPE:
+        case OP_SB_TYPE:
+        case OP_U_TYPE:
+        case OP_UJ_TYPE:
+        case OP_SHIFT:
+        case OP_FENCE:
+        case OP_FFFF:
+        case OP_READ:
+        case OP_LR:
+        case OP_STD_EXT:
+        case OP_R_FP1:
+        case OP_R_FP2:
+        case OP_R_FP3:
+        case OP_R_FP2_RM:
+        case OP_R_FP3_RM:
+        case OP_R_FP4_RM:
+        default:
+          strcpy(instruction, "???");
+          break;
+      }
+
+      return 4;
+    }
+
+    n++;
+  }
+
+  strcpy(instruction, "???");
 
   return -1;
 }
 
 void list_output_riscv(struct _asm_context *asm_context, uint32_t start, uint32_t end)
 {
-  int cycles_min, cycles_max;
+  int cycles_min,cycles_max;
   char instruction[128];
-  char bytes[10];
-  int count;
-  int n;
+  uint32_t opcode;
 
   fprintf(asm_context->list, "\n");
 
   while(start < end)
   {
-    count = disasm_riscv(&asm_context->memory, start, instruction, &cycles_min, &cycles_max);
+    opcode = memory_read32_m(&asm_context->memory, start);
 
-    bytes[0] = 0;
-    for (n = 0; n < count; n++)
-    {
-      char temp[4];
-      sprintf(temp, "%02x ", memory_read_m(&asm_context->memory, start + n));
-      strcat(bytes, temp);
-    }
+    disasm_riscv(&asm_context->memory, start, instruction, &cycles_min, &cycles_max);
 
-    fprintf(asm_context->list, "0x%04x: %-9s %-40s cycles: ", start, bytes, instruction);
+    fprintf(asm_context->list, "0x%08x: 0x%08x %-40s cycles: ", start, opcode, instruction);
 
     if (cycles_min == cycles_max)
     { fprintf(asm_context->list, "%d\n", cycles_min); }
       else
     { fprintf(asm_context->list, "%d-%d\n", cycles_min, cycles_max); }
 
-    start += count;
+    start += 4;
   }
 }
 
