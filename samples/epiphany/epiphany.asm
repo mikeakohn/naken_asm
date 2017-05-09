22b56c8e8fc8ff85deb6683c33bfdf48453a488e
Michael Kohn <mike@mikekohn.net>
2017-05-08 19:02:00 -0500

    Adding a new epiphany example.

---
+++ samples/epiphany/epiphany.asm
@@ -0,0 +1,46 @@
+.epiphany
+
+.org 0
+.org 0x00
+  b _sync_interrupt
+.org 0x04
+  b _sw_exception_interrupt
+.org 0x08
+  b _memory_fault_interrupt
+.org 0x0c
+  b _timer0_interrupt
+.org 0x10
+  b _timer1_interrupt
+.org 0x14
+  b _message_interrupt
+.org 0x18
+  b _dma0_interrupt
+.org 0x1c
+  b _dma1_interrupt
+.org 0x20
+  b _wand_interrupt
+.org 0x24
+  b _user_interrupt
+
+
+.org 0x40
+_sync_interrupt:
+
+main:
+  mov v1, #0
+loop:
+  add v1, v1, #1
+  b loop
+
+_sw_exception_interrupt:
+_memory_fault_interrupt:
+_timer0_interrupt:
+_timer1_interrupt:
+_message_interrupt:
+_dma0_interrupt:
+_dma1_interrupt:
+_wand_interrupt:
+_user_interrupt:
+  rti
+
+
