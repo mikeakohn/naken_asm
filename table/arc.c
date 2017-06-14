de7884951208670672cb0919d60ec0bb4336f751
Michael Kohn <mike@mikekohn.net>
2017-06-13 20:37:44 -0500

    Adding abss instructions.

--- table/arc.c
+++ table/arc.c
@@ -21,6 +21,12 @@ struct _table_arc table_arc[] =
   { "abs",  0x206f0009, 0xf8ff003f, OP_B_U6 },
   { "abs",  0x202f0f89, 0xf8ff0fff, OP_B_LIMM },
   { "abs",  0x202f0009, 0xf8ff003f, OP_B_C },
+  { "abss",  0x2e6f7005, 0xffff703f, OP_0_U6 },
+  { "abss",  0x2e2f7f85, 0xffff7fff, OP_0_LIMM },
+  { "abss",  0x2e2f7005, 0xffff703f, OP_0_C },
+  { "abss",  0x286f0005, 0xf8ff003f, OP_B_U6 },
+  { "abss",  0x282f0f85, 0xf8ff0fff, OP_B_LIMM },
+  { "abss",  0x282f0005, 0xf8ff003f, OP_B_C },
 };
 
 struct _table_arc16 table_arc16[] =
