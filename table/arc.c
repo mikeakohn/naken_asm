e7b23e66eb8eafddf03f89a7582f0dcc91d266f4
Michael Kohn <mike@mikekohn.net>
2017-06-13 20:45:01 -0500

    Add abssw instructions.

--- table/arc.c
+++ table/arc.c
@@ -15,18 +15,24 @@
 
 struct _table_arc table_arc[] =
 {
-  { "abs",  0x266f7009, 0xffff703f, OP_0_U6 },
-  { "abs",  0x262f7f89, 0xffff7fff, OP_0_LIMM },
-  { "abs",  0x262f7009, 0xffff703f, OP_0_C },
-  { "abs",  0x206f0009, 0xf8ff003f, OP_B_U6 },
-  { "abs",  0x202f0f89, 0xf8ff0fff, OP_B_LIMM },
-  { "abs",  0x202f0009, 0xf8ff003f, OP_B_C },
-  { "abss",  0x2e6f7005, 0xffff703f, OP_0_U6 },
-  { "abss",  0x2e2f7f85, 0xffff7fff, OP_0_LIMM },
-  { "abss",  0x2e2f7005, 0xffff703f, OP_0_C },
-  { "abss",  0x286f0005, 0xf8ff003f, OP_B_U6 },
-  { "abss",  0x282f0f85, 0xf8ff0fff, OP_B_LIMM },
-  { "abss",  0x282f0005, 0xf8ff003f, OP_B_C },
+  { "abs",    0x266f7009, 0xffff703f, OP_0_U6 },
+  { "abs",    0x262f7f89, 0xffff7fff, OP_0_LIMM },
+  { "abs",    0x262f7009, 0xffff703f, OP_0_C },
+  { "abs",    0x206f0009, 0xf8ff003f, OP_B_U6 },
+  { "abs",    0x202f0f89, 0xf8ff0fff, OP_B_LIMM },
+  { "abs",    0x202f0009, 0xf8ff003f, OP_B_C },
+  { "abss",   0x2e6f7005, 0xffff703f, OP_0_U6 },
+  { "abss",   0x2e2f7f85, 0xffff7fff, OP_0_LIMM },
+  { "abss",   0x2e2f7005, 0xffff703f, OP_0_C },
+  { "abss",   0x286f0005, 0xf8ff003f, OP_B_U6 },
+  { "abss",   0x282f0f85, 0xf8ff0fff, OP_B_LIMM },
+  { "abss",   0x282f0005, 0xf8ff003f, OP_B_C },
+  { "abssw",  0x2e6f7004, 0xffff703f, OP_0_U6 },
+  { "abssw",  0x2e2f7f84, 0xffff7fff, OP_0_LIMM },
+  { "abssw",  0x2e2f7004, 0xffff703f, OP_0_C },
+  { "abssw",  0x286f0004, 0xf8ff003f, OP_B_U6 },
+  { "abssw",  0x2e2f7f84, 0xffff7fff, OP_B_LIMM },
+  { "abssw",  0x282f0004, 0xf8ff003f, OP_B_C },
 };
 
 struct _table_arc16 table_arc16[] =
