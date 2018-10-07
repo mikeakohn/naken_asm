20c5d4d6838ba6615e809a97b001bede0faff115
Michael Kohn <mike@mikekohn.net>
2018-10-07 07:02:08 -0500

    Deal with possible space after end of macro defined value.

--- common/ifdef_expression.c
+++ common/ifdef_expression.c
@@ -111,15 +111,23 @@ int ret;
   }
 
   tokens_get(asm_context, token, TOKENLEN);
+
   if (macros_lookup(&asm_context->macros, token, &param_count) != NULL)
-  { ret = 1; }
+  {
+    ret = 1;
+  }
     else
   if (symbols_find(&asm_context->symbols, token) != NULL)
-  { ret = 1; }
+  {
+    ret = 1;
+  }
     else
-  { ret = 0; }
+  {
+    ret = 0;
+  }
 
   tokens_get(asm_context, token, TOKENLEN);
+
   if (IS_NOT_TOKEN(token,')'))
   {
     print_error_unexp(token, asm_context);
@@ -138,7 +146,7 @@ printf("debug> #if eval_operation()  operator=%d  num1=%d  num2=%d\n", operator,
   switch(operator)
   {
     case OPER_EQUAL:
-      return num1 ==num2 ? 1 : 0;
+      return num1 == num2 ? 1 : 0;
     case OPER_GT_EQUAL:
       return num1 >= num2 ? 1 : 0;
     case OPER_LT_EQUAL:
@@ -158,22 +166,37 @@ printf("debug> #if eval_operation()  operator=%d  num1=%d  num2=%d\n", operator,
   return -1;
 }
 
-static int is_num(char *s)
+static int is_num(char *value)
 {
-  while(*s != 0) { if (*s < '0' || *s > '9') return 0; s++; }
+  char *s = value;
+
+  while(*s != 0)
+  {
+    // If there is white space at the end of the value then remove it.
+    // This is due to do adding an extra space at the end of macros.. should
+    // probably change this.
+    if (*s == ' ' && s != value)
+    {
+      while(*s == ' ') { s++; }
+      return (*s == 0) ? 1 : 0;
+    }
+
+    if (*s < '0' || *s > '9') { return 0; }
+    s++;
+  }
+
   return 1;
 }
 
 static int parse_ifdef_expression(struct _asm_context *asm_context, int *num, int paren_count, int precedence, int state)
 {
-char token[TOKENLEN];
-struct _operator operator;
-struct _symbols_data *symbols_data;
-int token_type;
-//int state=0;  // 0 = get next num to process
-int not = 0;
-int n1 = 0;
-int n;
+  char token[TOKENLEN];
+  struct _operator operator;
+  struct _symbols_data *symbols_data;
+  int token_type;
+  int not = 0;
+  int n1 = 0;
+  int n;
 
   operator.operation = OPER_NONE;
   operator.precedence = precedence;
@@ -233,7 +256,10 @@ printf("debug> #if eval_operation() @EOL  n=%d precedence=%d state=%d\n", n, pre
           else
         if (IS_TOKEN(token,'('))
         {
-          if (parse_ifdef_expression(asm_context, &n, paren_count+1, PREC_OR, 0) == -1) return -1;
+          if (parse_ifdef_expression(asm_context, &n, paren_count + 1, PREC_OR, 0) == -1)
+          {
+            return -1;
+          }
         }
           else
         if (IS_TOKEN(token,')'))
@@ -275,7 +301,7 @@ printf("debug> #if eval_operation() @paren  n=%d\n", n);
 #ifdef DEBUG
 printf("debug> #if: parse_defined()=%d\n", n);
 #endif
-          if (n == -1) return -1;
+          if (n == -1) { return -1; }
         }
           else
         if (value == NULL && (symbols_data = symbols_find(&asm_context->symbols, token)) != NULL)
@@ -364,15 +390,19 @@ printf("debug> #if eval_operation() @ state 2  n=%d\n", n);
 
 int eval_ifdef_expression(struct _asm_context *asm_context)
 {
-char token[TOKENLEN];
-int token_type;
-int num = 0;
+  char token[TOKENLEN];
+  int token_type;
+  int num = 0;
 
-  if (parse_ifdef_expression(asm_context, &num, 0, PREC_OR, 0) == -1) return -1;
+  if (parse_ifdef_expression(asm_context, &num, 0, PREC_OR, 0) == -1)
+  {
+    return -1;
+  }
 
 #ifdef DEBUG
 printf("debug> parse_ifdef_expression() result is %d\n", num);
 #endif
+
   token_type = tokens_get(asm_context, token, TOKENLEN);
 
   if (token_type != TOKEN_EOL && token_type != TOKEN_EOF)
