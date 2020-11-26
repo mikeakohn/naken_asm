4e36fcbcb76d1150b9f6e7fbbeceb9bb90db771c
Michael Kohn <mike@mikekohn.net>
2020-11-26 10:16:03 -0600

    Clean up Dreamcast sample.

---
+++ samples/dreamcast/IP.asm
@@ -0,0 +1,44 @@
+.org 0
+
+.binfile "/tmp/IP.TMPL"
+
+.org 0
+
+;; Hardware ID
+.db "SEGA SEGAKATANA "
+
+;; Maker ID
+.db "SEGA ENTERPRISES"
+
+;; Device Information
+.db "0000 GD-ROM1/1  "
+
+;; Area Symbols (J=Japan, U=USA/Canada, E=Europe)
+.db "JUE     "
+
+;; Peripherals
+.db "1fff000 "
+
+;; Product number / Product version
+.db "HDR-0000  "
+.db "      "
+
+;; Release date
+.db "20200307        "
+
+;; Boot filename
+.db "1ST_READ.BIN    "
+
+;; Name of the company that produced the disc
+.db "naken_asm       "
+
+;; Name of the software
+.db "Dreamcast Test  "
+.db "                "
+.db "                "
+.db "                "
+.db "                "
+.db "                "
+.db "                "
+.db "                "
+
