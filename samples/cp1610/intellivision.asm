36eb84829e2ff2413ff3115143896f894f517f37
Michael Kohn <mike@mikekohn.net>
2021-02-13 06:35:34 -0600

    Intellivision: Adding interrupt code to sample.

--- samples/cp1610/intellivision.asm
+++ samples/cp1610/intellivision.asm
@@ -26,7 +26,54 @@ title:
   .dw 107, 'H', 'e', 'l', 'l', 'o', 0
 
 main:
+  ;; Setup interrupt.
+  mvii #vblank_interrupt, r0
+  mvo r0, 0x100
+  swap r0
+  mvo r0, 0x101
+  ;; Enable interrupts.
   eis
+
 while_1:
+  ;; Set character on the screen to white 'A'.
+  mvii #(0x21 << 3) | 7, r0
+  mvii #0x200, r1
+  mvo@ r0, r1
+
+  jsr r4, wait_for_1_second
+
+  ;; Set character on the screen to blue 'Z'.
+  mvii #(0x3a << 3) | 1, r0
+  mvii #0x200, r1
+  mvo@ r0, r1
+
+  jsr r4, wait_for_1_second
+
   b while_1
+
+wait_for_1_second:
+  pshr r4
+  ;; Count 60 interrupts to delay 1 second.
+  mvii #60, r1
+wait_for_1_second_loop:
+  ;; Set did_vblank to 0.
+  mvii #0, r0
+  mvo r0, 0x0102
+  ;; Wait for did_vblank to change to 1.
+wait_on_interrupt:
+  mvi 0x102, r0
+  cmpi #1, r0
+  bneq wait_on_interrupt
+  decr r1
+  bneq wait_for_1_second_loop
+  pulr r7
+
+vblank_interrupt:
+  ;; Enable display.
+  mvo r0, 0x0020
+  ;; Set did_vblank to 1.
+  mvii #1, r0
+  mvo r0, 0x0102
+  ;; Return from interrupt.
+  jr r5
 
