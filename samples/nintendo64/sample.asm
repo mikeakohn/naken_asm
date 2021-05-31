2364f5441fae1ff5fbe285f895a79c9c2b1c143f
Michael Kohn <mike@mikekohn.net>
2021-05-31 08:00:48 -0500

    Adding more comments to Nintendo 64 sample code to make it easier to read.

--- samples/nintendo64/sample.asm
+++ samples/nintendo64/sample.asm
@@ -1,3 +1,12 @@
+;; Simple Nintendo 64 sample.
+;;
+;; Copyright 2021 - By Michael Kohn
+;; http://www.mikekohn.net/
+;; mike@mikekohn.net
+;;
+;; Sets up the Nintendo 64 display, clear parts of the screen with a color,
+;; draws some squares and triangles using the hardware.
+
 .mips
 
 .include "nintendo64/rdp.inc"
@@ -69,18 +78,32 @@ start:
   mtc0 $t0, CP0_STATUS
 .endif
 
-  ;; Setup display memory.
+  ;; Color the first 100 scan lines blue.
+  ;; Color Bits: rrrr_rggg_ggbb_bbba (0000_0000_0011_1110).
   li $a0, KSEG1 | 0x0010_0000
-  li $t0, 0xff
+  li $t0, 0x003e
   li $t1, 320 * 100
-fill_loop:
+fill_loop_top:
   sh $t0, 0($a0)
   addiu $a0, $a0, 2
   addiu $t1, $t1, -1
-  bne $t1, $0, fill_loop
+  bne $t1, $0, fill_loop_top
   nop
 
-  ;; Clear some memory.
+  ;; Color the last 140 scan lines brown.
+  ;; Color Bits: rrrr_rggg_ggbb_bbba (0011_1000_1100_0010).
+  li $t0, 0x38c2
+  li $t1, 320 * 140
+fill_loop_bottom:
+  sh $t0, 0($a0)
+  addiu $a0, $a0, 2
+  addiu $t1, $t1, -1
+  bne $t1, $0, fill_loop_bottom
+  nop
+
+  ;; This reads reads in a set of [ 32 bit address, 32 bit value ]
+  ;; from ROM memory. For each 32 bit value, it is written to the
+  ;; 32 bit address which sets up the video display.
 setup_video:
   li $t0, ntsc_320x240x16
   li $t1, (ntsc_320x240x16_end - ntsc_320x240x16) / 8
@@ -93,7 +116,7 @@ setup_video_loop:
   bne $t1, $0, setup_video_loop
   nop
 
-  ;; Setup RDP to pull instructions from RSP DMEM.
+  ;; Setup RDP to execute instructions from RSP DMEM.
   li $a0, KSEG1 | DP_BASE
   li $t0, 0x0000
   sw $t0, DP_START_REG($a0)
@@ -101,7 +124,7 @@ setup_video_loop:
   li $t0, 0x0006
   sw $t0, DP_STATUS_REG($a0)
 
-  ;; Copy RDP instructions to RSP data memory.
+  ;; Copy RDP instructions from ROM to RSP data memory.
 setup_rdp:
   li $t0, dp_setup
   li $t1, (dp_draw_triangles_end - dp_setup) / 8
@@ -115,11 +138,13 @@ setup_rdp_loop:
   bne $t1, $0, setup_rdp_loop
   nop
 
-  ;; Start RDP executing instructions.
+  ;; Start RDP executing instructions. This should draw the triangles
+  ;; defined in the ROM area below.
   li $a0, KSEG1 | DP_BASE
   li $t0, dp_draw_triangles_end - dp_setup
   sw $t0, DP_END_REG($a0)
 
+  ;; Inifinite loop at end of program.
 while_1:
   beq $0, $0, while_1
   nop
@@ -127,6 +152,7 @@ while_1:
 ;; NTSC values found in the Reality Coprocessor.pdf
 ;; VI_CONTROL_REG:                   0 0011 0010 0000 1110
 ;; VI_TIMING_REG:  0000 0011 1110 0101 0010 0010 0011 1001
+;; VI_BASE is the video interface base in KSEG1 (no TLB, no cache).
 ntsc_320x240x16:
   .dc32 KSEG1 | VI_BASE | VI_CONTROL_REG,     0x0000_320e
   .dc32 KSEG1 | VI_BASE | VI_DRAM_ADDR_REG,   0xa010_0000
@@ -143,6 +169,11 @@ ntsc_320x240x16:
   .dc32 KSEG1 | VI_BASE | VI_Y_SCALE_REG,     0x0000_0400
 ntsc_320x240x16_end:
 
+;; Here are some DP (Display Processor) hardware commands that
+;; set up the drawing modes and draw some different colored shapes.
+;; The code to calculate the slopes and such for the triangles are
+;; done with the calc_triangle.py Python script.
+.align_bits 64
 dp_setup:
   .dc64 (DP_OP_SET_COLOR_IMAGE << 56) | (2 << 51) | (319 << 32) | 0x10_0000
   .dc64 (DP_OP_SET_Z_IMAGE << 56) | (0x10_0000 * (320 * 200 * 2))
@@ -151,32 +182,34 @@ dp_setup:
 dp_setup_end:
 
 dp_draw_squares:
+  ;; Red square.
   .dc64 (DP_OP_SET_FILL_COLOR << 56) | (0xf80e << 16) | (0xf80e)
   .dc64 (DP_OP_FILL_RECTANGLE << 56) | ((100 << 2) << 44) | ((100 << 2) << 32) | ((50 << 2) << 12) | (50 << 2)
+  ;; Dark purple square.
   .dc64 (DP_OP_SET_FILL_COLOR << 56) | (0x080f << 16) | (0x080f)
   .dc64 (DP_OP_FILL_RECTANGLE << 56) | ((150 << 2) << 44) | ((150 << 2) << 32) | ((100 << 2) << 12) | (100 << 2)
 dp_draw_squares_end:
 
 dp_draw_triangles:
-  ;; Left major triangle
+  ;; Left major triangle (green)
   .dc64 (DP_OP_SET_FILL_COLOR << 56) | (0x07c0 << 16) | (0x07c0)
   .dc64 0x088002fb02aa01e0
   .dc64 0x00aa0000fffd097b
   .dc64 0x009615c1ffff6ef5
   .dc64 0x0095f0bf000065b0
-  ;; Right major triangle
+  ;; Right major triangle (purple)
   .dc64 (DP_OP_SET_FILL_COLOR << 56) | (0xf83e << 16) | (0xf83e)
   .dc64 0x080002fb02aa01e0
   .dc64 0x00e600000002f684
   .dc64 0x00f9ea3e0000910a
   .dc64 0x00fa0f40ffff9a4f
-  ;; Isosceles triangle
+  ;; Isosceles triangle (white)
   .dc64 (DP_OP_SET_FILL_COLOR << 56) | (0xfffe << 16) | (0xfffe)
   .dc64 0x08000168016800c8
   .dc64 0x00e6000017d77bffffff
   .dc64 0x00f9eccc00007fff
   .dc64 0x00fa1333ffff8000
-  ;; Isosceles triangle (upside-down)
+  ;; Isosceles triangle (yellow, upside-down)
   .dc64 (DP_OP_SET_FILL_COLOR << 56) | (0xffc0 << 16) | (0xffc0)
   .dc64 0x0880016800c900c8
   .dc64 0x00aa0000ffff7f84
