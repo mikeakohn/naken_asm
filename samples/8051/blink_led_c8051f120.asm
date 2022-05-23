615ed4a70189ea4c19fcba11a8a46e8d41650e55
Cjacker <jianzhong.huang@i-soft.com.cn>
2022-05-23 16:32:39 +0800

    8051/8052: add led blink example for c8051f120

---
+++ samples/8051/blink_led_c8051f120.asm
@@ -0,0 +1,48 @@
+;; c8051f120 sample
+;;
+;; Copyright 2022 - By Cjacker Huang
+;; cjacker@gmail.com
+;;
+;; This is a small example for Silicon Labs' c8051f120 development board.
+;; The demo will blink a led wire to P3.3.
+
+.8051
+
+.include "c8051f120.inc"
+
+LED equ P3.3
+
+org 0
+start:
+  ; disable watchdog
+  clr EA
+  mov WDTCN, #0xde
+  mov WDTCN, #0xad
+  setb EA
+
+  ; setup SFRPAGE to 0x0f
+  mov SFRPAGE, #CONFIG_PAGE
+
+  ; enable Crossbar 
+  mov XBR2, #0x40
+
+  ; set P3.3 to Push-Pull 
+  mov P3MDOUT, #0x08
+ 
+  setb LED 
+  loop:
+    cpl LED
+    lcall delay
+    sjmp loop
+
+delay:
+    mov r3, #255
+    outer:
+      mov r2, #255
+      inner:
+        nop
+        djnz r2, inner
+      djnz r3, outer
+  ret
+
+end
