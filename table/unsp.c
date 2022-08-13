9dbe70cfbb632e2d6d2ff87381b7e7ff03ccae91
Michael Kohn <mike@mikekohn.net>
2022-08-13 09:45:10 -0500

    Issue #108: Opcode mask for jump instructions should be 0xff80 (6 bits of offset and 1 bit of direction).

--- table/unsp.c
+++ table/unsp.c
@@ -5,13 +5,15 @@
  *     Web: http://www.mikekohn.net/
  * License: GPLv3
  *
- * Copyright 2010-2021 by Michael Kohn
+ * Copyright 2010-2022 by Michael Kohn
  *
  */
 
 #include <stdlib.h>
 
 #include "table/unsp.h"
+
+// http://vtech.pulkomandy.tk/doku.php?id=instruction_encoding
 
 // Note: In the documentation used here, some opcodes had ? in their
 // bitfields. These are 0'd with a mask set to 1.
@@ -37,35 +39,35 @@ struct _table_unsp table_unsp[] = {
   { "mac.ss",      0xf180, 0xf180, UNSP_OP_MAC },
   { "mac.us",      0xf080, 0xf180, UNSP_OP_MAC },
   //{ "mac.uu",      0xe080, 0xf180, UNSP_OP_MAC },
-  { "jb",          0x0e00, 0xffc0, UNSP_OP_JMP },
-  { "jcc",         0x0e00, 0xffc0, UNSP_OP_JMP },
-  { "jnae",        0x0e00, 0xffc0, UNSP_OP_JMP },
-  { "jae",         0x1e00, 0xffc0, UNSP_OP_JMP },
-  { "jcs",         0x1e00, 0xffc0, UNSP_OP_JMP },
-  { "jnb",         0x1e00, 0xffc0, UNSP_OP_JMP },
-  { "jge",         0x2e00, 0xffc0, UNSP_OP_JMP },
-  { "jsc",         0x2e00, 0xffc0, UNSP_OP_JMP },
-  { "jnl",         0x2e00, 0xffc0, UNSP_OP_JMP },
-  { "jl",          0x3e00, 0xffc0, UNSP_OP_JMP },
-  { "jss",         0x3e00, 0xffc0, UNSP_OP_JMP },
-  { "jnge",        0x3e00, 0xffc0, UNSP_OP_JMP },
-  { "jne",         0x4e00, 0xffc0, UNSP_OP_JMP },
-  { "jnz",         0x4e00, 0xffc0, UNSP_OP_JMP },
-  { "je",          0x5e00, 0xffc0, UNSP_OP_JMP },
-  { "jz",          0x5e00, 0xffc0, UNSP_OP_JMP },
-  { "jpl",         0x6e00, 0xffc0, UNSP_OP_JMP },
-  { "jmi",         0x7e00, 0xffc0, UNSP_OP_JMP },
-  { "jbe",         0x8e00, 0xffc0, UNSP_OP_JMP },
-  { "jna",         0x8e00, 0xffc0, UNSP_OP_JMP },
-  { "ja",          0x9e00, 0xffc0, UNSP_OP_JMP },
-  { "jnbe",        0x9e00, 0xffc0, UNSP_OP_JMP },
-  { "jle",         0xae00, 0xffc0, UNSP_OP_JMP },
-  { "jng",         0xae00, 0xffc0, UNSP_OP_JMP },
-  { "jg",          0xbe00, 0xffc0, UNSP_OP_JMP },
-  { "jnle",        0xbe00, 0xffc0, UNSP_OP_JMP },
-  { "jvc",         0xce00, 0xffc0, UNSP_OP_JMP },
-  { "jvs",         0xde00, 0xffc0, UNSP_OP_JMP },
-  { "jmp",         0xee00, 0xffc0, UNSP_OP_JMP },
+  { "jb",          0x0e00, 0xff80, UNSP_OP_JMP },
+  { "jcc",         0x0e00, 0xff80, UNSP_OP_JMP },
+  { "jnae",        0x0e00, 0xff80, UNSP_OP_JMP },
+  { "jae",         0x1e00, 0xff80, UNSP_OP_JMP },
+  { "jcs",         0x1e00, 0xff80, UNSP_OP_JMP },
+  { "jnb",         0x1e00, 0xff80, UNSP_OP_JMP },
+  { "jge",         0x2e00, 0xff80, UNSP_OP_JMP },
+  { "jsc",         0x2e00, 0xff80, UNSP_OP_JMP },
+  { "jnl",         0x2e00, 0xff80, UNSP_OP_JMP },
+  { "jl",          0x3e00, 0xff80, UNSP_OP_JMP },
+  { "jss",         0x3e00, 0xff80, UNSP_OP_JMP },
+  { "jnge",        0x3e00, 0xff80, UNSP_OP_JMP },
+  { "jne",         0x4e00, 0xff80, UNSP_OP_JMP },
+  { "jnz",         0x4e00, 0xff80, UNSP_OP_JMP },
+  { "je",          0x5e00, 0xff80, UNSP_OP_JMP },
+  { "jz",          0x5e00, 0xff80, UNSP_OP_JMP },
+  { "jpl",         0x6e00, 0xff80, UNSP_OP_JMP },
+  { "jmi",         0x7e00, 0xff80, UNSP_OP_JMP },
+  { "jbe",         0x8e00, 0xff80, UNSP_OP_JMP },
+  { "jna",         0x8e00, 0xff80, UNSP_OP_JMP },
+  { "ja",          0x9e00, 0xff80, UNSP_OP_JMP },
+  { "jnbe",        0x9e00, 0xff80, UNSP_OP_JMP },
+  { "jle",         0xae00, 0xff80, UNSP_OP_JMP },
+  { "jng",         0xae00, 0xff80, UNSP_OP_JMP },
+  { "jg",          0xbe00, 0xff80, UNSP_OP_JMP },
+  { "jnle",        0xbe00, 0xff80, UNSP_OP_JMP },
+  { "jvc",         0xce00, 0xff80, UNSP_OP_JMP },
+  { "jvs",         0xde00, 0xff80, UNSP_OP_JMP },
+  { "jmp",         0xee00, 0xff80, UNSP_OP_JMP },
   { "retf",        0x9a90, 0xffff, UNSP_OP_NONE },
   { "reti",        0x9a98, 0xffff, UNSP_OP_NONE },
   { "pop",         0x9080, 0xf1c0, UNSP_OP_POP },
