6a80a336dcb9c78e67314c2d142ea67fdfc88d82
adabz <adaab@oregonstate.edu>
2022-11-30 22:40:51 -0800

    8008.h: adjusted registers definition

--- simulate/8008.h
+++ simulate/8008.h
@@ -20,16 +20,11 @@
 struct _simulate_8008
 {
   // 7 Registers (A, B, C, D, E, H, L)
-  uint8_t reg_a, reg_b, reg_c, reg_d, reg_e;
-  uint8_t reg_h, reg_l;
+  uint8_t reg_a, reg_b, reg_c, reg_d, reg_e, reg_h, reg_l;
 
-  // stack pointer
-  int sp:14;
+  // stack pointer / program counter
+  unsigned int sp : 14, pc : 14;
   
-
-  // program counter
-  int pc:14;
-
 };
 
 struct _simulate *simulate_init_8008();
