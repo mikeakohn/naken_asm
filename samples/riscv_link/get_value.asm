f0921dd71e58f081bfb90f97150b9974fec10ab9
Michael Kohn <mike@mikekohn.net>
2024-01-02 17:00:47 -0600

    riscv: Add sample program for VisionFive, link assembly code with C.

---
+++ samples/riscv_link/get_value.asm
@@ -0,0 +1,8 @@
+.riscv64
+
+.export add_nums
+
+add_nums:
+  add a0, a0, a1
+  ret
+
---
+++ samples/riscv_link/test.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int add_nums(int a, int b);
+
+int main(int argc, char *argv[])
+{
+  if (argc != 3)
+  {
+    printf("Usage: ./test <num> <num>\n");
+    exit(0);
+  }
+
+  int a = atoi(argv[1]);
+  int b = atoi(argv[2]);
+
+  int c = add_nums(a, b);
+
+  printf("%d + %d = %d\n", a, b, c);
+
+  return 0;
+}
+
