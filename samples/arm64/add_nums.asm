0a22c17ab74fd75ff0c6de3e0b711133a10834a4
Michael Kohn <mike@mikekohn.net>
2024-01-07 17:14:56 -0600

    arm64: Adding new sample.

---
+++ samples/arm64/add_nums.asm
@@ -0,0 +1,7 @@
+.arm64
+.export add_nums
+
+add_nums:
+  add w0, w0, w1
+  ret
+
---
+++ samples/arm64/test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int add_nums(int a, int b);
+
+void nothing()
+{
+  printf("nothing\n");
+}
+
+int main(int argc, char *argv[])
+{
+  nothing();
+
+  return 0;
+}
+
